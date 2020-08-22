2f5deba70cc00ec6330152ef995671cf68d1d788
a2htray <a2htray.yuen@gmail.com>
2020-08-22 12:43:00 +0800

    新增三个目标函数，及补充简单 DE 算法说明

--- doc/FUNCTIONS.md
+++ doc/FUNCTIONS.md
@@ -13,6 +13,19 @@ Rotated Rastrigin's Function [1]
 
 ![](./assets/QQ截图20200821123342.png)
 
+Hyper-Ellipsoid Function [2]
+
+![](./assets/QQ截图20200822115726.png)
+
+Katsuura's Function [2]
+
+![](./assets/QQ截图20200822120028.png)
+
+Griewangk's function [2]
+
+![](./assets/QQ截图20200822123055.png)
+
 ### 参考
 
 1. Tang, K., Yao, X., Suganthan, P. N., MacNish, C., Chen, Y. P., Chen, C. M., & Yang, Z. (2010). Benchmark Functions for the CEC 2010 Special Session and Competition on Large Scale Global Optimization. University of Science and Technology of China (USTC), School of Computer Science and Technology, Nature Inspired Computation and Applications Laboratory (NICAL), Hefei, Anhui. China. Tech. Rep, Tech. Rep.
+2. Storn, R., & Price, K. (1997). Differential evolution–a simple and efficient heuristic for global optimization over continuous spaces. Journal of global optimization, 11(4), 341-359.
--- functions.go
+++ functions.go
@@ -16,7 +16,7 @@ func ObjectRotatedElliptic(float64s []float64) float64 {
 	d := float64(len(float64s))
 	ret := float64(0)
 	for i, v := range float64s {
-		ret += math.Pow(v, 2) * math.Pow(1000000.0, (float64(i)-1)/(d-1))
+		ret += math.Pow(v, 2) * math.Pow(1000000.0, (float64(i))/(d-1))
 	}
 	return 0
 }
@@ -28,4 +28,50 @@ func ObjectRotatedRastrigin(float64s []float64) float64 {
 		ret += math.Pow(v, 2) - 10*math.Cos(2*math.Pi*v) + 10
 	}
 	return ret
+}
+
+// ObjectHyperEllipsoid Hyper-Ellipsoid
+func ObjectHyperEllipsoid(float64s []float64) float64 {
+	ret := float64(0)
+	for i, v := range float64s {
+		ret += math.Pow(float64(i+1), 2) * math.Pow(v, 2)
+	}
+	return ret
+}
+
+// ObjectKatsuura Katsuura's Function
+func ObjectKatsuura(beta int) func([]float64) float64 {
+	return func(float64s []float64) float64 {
+		ret := float64(1)
+
+		for i, v := range float64s {
+			j := float64(i + 1)
+			part := float64(0)
+
+			for k := 0; k <= beta; k++ {
+				a := math.Pow(2, float64(k)) * v
+				part += math.Abs(a-float64(FindNearestInt(a))) * math.Pow(2, -float64(k))
+			}
+
+			ret *= 1 + j*part
+		}
+
+		return ret
+	}
+}
+
+// ObjectGriewangk Griewangk’s function
+func ObjectGriewangk(float64s []float64) float64 {
+	a := float64(0)
+	for _, v := range float64s {
+		a += math.Pow(v, 2)
+	}
+	a = a / 4000
+
+	b := float64(1)
+	for i, v := range float64s {
+		b *= math.Cos(v / (float64(i) + 1))
+	}
+
+	return a - b + 1
 }
